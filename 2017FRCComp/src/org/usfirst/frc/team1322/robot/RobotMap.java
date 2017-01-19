ed2b4641f730c7181134fba84c12f44a7e60cc7c
jovanus <eric.weber.r@gmail.com>
2017-01-18 20:05:17 -0500

    Initial Commit

---
+++ 2017FRCComp/src/org/usfirst/frc/team1322/robot/RobotMap.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team1322.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	
+	// CANs
+	public static final int CAN_D_FL = 1;
+	public static final int CAN_D_RL = 2;
+	public static final int CAN_D_FR = 3;
+	public static final int CAN_D_RR = 4;
+	
+	// USB Controllers
+	public static final int USB_Driver = 0;
+	public static final int USB_AUX = 1;
+}
