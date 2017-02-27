405f6ebaac0c2838ae8845d5ff8c365449d1c486
SPZProductions <SPZProductions@SPZ-Productions>
2017-02-26 17:28:24 -0800

    Added Adafruit IMU code

--- 2017FRCComp/src/org/usfirst/frc/team1322/robot/subsystems/DriveSubsystem.java
+++ 2017FRCComp/src/org/usfirst/frc/team1322/robot/subsystems/DriveSubsystem.java
@@ -3,10 +3,10 @@ package org.usfirst.frc.team1322.robot.subsystems;
 import org.usfirst.frc.team1322.robot.RobotMap;
 import org.usfirst.frc.team1322.robot.commands.TC_DriveSystem;
 import com.ctre.CANTalon;
-import com.ctre.CANTalon.TalonControlMode;
-
+import org.usfirst.frc.team1322.robot.utils.BNO055;
+import org.usfirst.frc.team1322.robot.utils.BNO055.CalData;
+import org.usfirst.frc.team1322.robot.utils.BNO055.reg_t;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 /**
@@ -15,16 +15,14 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 public class DriveSubsystem extends Subsystem {
     private RobotDrive DriveSystem;
     private CANTalon m_CAN_D_FL, m_CAN_D_RL, m_CAN_D_FR, m_CAN_D_RR;
-    private Servo shifterL, shifterR;
     private int encoderValue;
-    private int setPosition;
     private boolean autonActivated;
-    private final static double scaleFactor = 8500.0;
+    private static BNO055 imu;
     
     
     public DriveSubsystem(){
-    	shifterL = new Servo(RobotMap.SHIFTER_L);
-    	shifterR = new Servo(RobotMap.SHIFTER_R);
+    	imu = BNO055.getInstance(BNO055.opmode_t.OPERATION_MODE_IMUPLUS,
+				BNO055.vector_type_t.VECTOR_EULER);
     	m_CAN_D_FL = new CANTalon(RobotMap.CAN_D_FL);
     	m_CAN_D_RL = new CANTalon(RobotMap.CAN_D_RL);
     	m_CAN_D_FR = new CANTalon(RobotMap.CAN_D_FR);
@@ -101,6 +99,67 @@ public class DriveSubsystem extends Subsystem {
 
 	public int getError() {
 		return (int)m_CAN_D_RR.getError();
+	}
+	
+	//EVERYTHING BENEATH THIS IS FOR THE GYRO
+	
+    /**
+	 * The heading of the sensor (x axis) in continuous format. Eg rotating the
+	 *   sensor clockwise two full rotations will return a value of 720 degrees.
+	 *
+	 * @return heading in degrees
+     */
+    public double getHeading() {
+    	return imu.getHeading();
+    }
+    
+    /**
+     * Gets a vector representing the sensors position (heading, roll, pitch).
+	 * heading:    0 to 360 degrees
+	 * roll:     -90 to +90 degrees
+	 * pitch:   -180 to +180 degrees
+	 *
+	 * For continuous rotation heading (doesn't roll over between 360/0) see
+	 *   the getHeading() method.
+	 *
+	 * @return a vector [heading, roll, pitch]
+	 */
+    public double[] getVector() {
+    	return imu.getVector();
+    }
+    
+	/**
+	 * @return true if the IMU is found on the I2C bus
+	 */
+	public boolean isSensorPresent() {
+		return imu.isSensorPresent();
+	}
+
+	/** 
+	 * @return true when the IMU is initialized.
+	 */
+	public boolean isInitialized() {
+		return imu.isInitialized();
+	}
+	
+	/**
+	 * Gets current IMU calibration state.
+	 * @return each value will be set to 0 if not calibrated, 3 if fully
+	 *   calibrated.
+	 */
+	public BNO055.CalData getCalibration() {
+		return imu.getCalibration();
+	}
+	
+	/**
+	 * Returns true if all required sensors (accelerometer, magnetometer,
+	 *   gyroscope) in the IMU have completed their respective calibration
+	 *   sequence.
+	 * @return true if calibration is complete for all sensors required for the
+	 *   mode the sensor is currently operating in. 
+	 */
+	public boolean isCalibrated() {
+		return imu.isCalibrated();
 	}
 
 }
